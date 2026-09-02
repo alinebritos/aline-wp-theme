94d3ec44ff2b61b119510512df7926b275f3f69d
Aline de Brito Silva <aline.debrito@gmail.com>
2026-09-01 21:51:47 -0300

    Teste de disparo da Action

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer class="site-footer">
   <p>© <?php echo esc_html(date('Y')); ?> <?php bloginfo('name'); ?>. Todos os direitos reservados.</p>
   <div class="social">
-    <a href="https://github.com/alinebritos/" target="_blank">Github2</a>
+    <a href="https://github.com/alinebritos/" target="_blank">Github234</a>
     <a href="https://www.linkedin.com/in/alinebritos/" target="_blank">LinkedIn2</a>
     <a href="https://www.behance.net/alinedbrito" target="_blank">Behance</a>
   </div>
